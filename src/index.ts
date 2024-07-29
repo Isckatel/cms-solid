fb46537b53093a05d4142edf13ae4841bfd931d3
i.egoshin <i.egoshin@redkit-lab.ru>
2024-07-29 12:24:25 +0500

    Убрал сущность Component

--- src/index.ts
+++ src/index.ts
@@ -5,49 +5,43 @@ interface IPlugin {
     render(): Promise<string>
 }
 
-//Компонент страницы:
-class Component {
-    constructor(public name: string, public params: object) {}
-}
-
-//Общий интерфейс для всех команд:
+// Общий интерфейс для всех команд:
 interface ICommand {
     execute(): Promise<string>
 }
 
-//Рендеринг компонента:
+// Рендеринг плагина:
 class RenderCommand implements ICommand {
     constructor(private plugin: IPlugin) {}
 
     async execute(): Promise<string> {
         return this.plugin.render()
-  }
+    }
 }
 
 class Page {
-    private components: Component[] = [];
+    private plugins: IPlugin[] = []
 
-    addComponent(component: Component) {
-        this.components.push(component)
+    addPlugin(plugin: IPlugin) {
+        this.plugins.push(plugin)
     }
 
     async render(): Promise<void> {
-        const bodyElement = document.querySelector('body')
+        const bodyElement = document.querySelector('body');
         const parser = new DOMParser()
 
-        for (const component of this.components) {
-            const plugin = this.getPlugin(component.name)
-            const command = new RenderCommand(plugin)
-            
+        for (const plugin of this.plugins) {
+            const command = new RenderCommand(plugin);
+
             // Выполняем команды, но не дожидаемся завершения
             command.execute().then(result => {
                 if (bodyElement) {
-                    const doc = parser.parseFromString(result, "text/html")
+                    const doc = parser.parseFromString(result, "text/html");
                     const element = doc.body.firstChild;
-                    if(element) {
-                        bodyElement.appendChild(element)
+                    if (element) {
+                        bodyElement.appendChild(element);
                     } else {
-                        console.log("Нет контента в компоненте или это не HTML-элемент")
+                        console.log("Нет контента в плагине или это не HTML-элемент");
                     }
                 }
             });
@@ -55,32 +49,32 @@ class Page {
     }
 
     private getPlugin(name: string): IPlugin {
-      //Получения плагина по имени с помощью DI
-      try {
-        return IoC.resolve(name,[])
-      } catch (error) {
-        throw new Error(`Plugin ${name} not found`)
-      }
+        // Получение плагина по имени с помощью DI
+        try {
+            return IoC.resolve(name, [])
+        } catch (error) {
+            throw new Error(`Plugin ${name} not found`)
+        }
     }
 }
 
 class Plugin1 implements IPlugin {
     name = 'hi1'
     world: string
-    constructor(world: string){
+    constructor(world: string) {
         this.world = world
     }
     async render(): Promise<string> {
-      return new Promise((resolve, reject) => {
-        resolve(`<p>Привет, ${this.world}!</p>`)
-      })
+        return new Promise((resolve, reject) => {
+            resolve(`<p>Привет, ${this.world}!</p>`)
+        })
     }
 }
 
 class Plugin2 implements IPlugin {
     name = 'hi2'
     world: string
-    constructor(world: string){
+    constructor(world: string) {
         this.world = world
     }
     async render(): Promise<string> {
@@ -91,13 +85,14 @@ class Plugin2 implements IPlugin {
 }
 
 const args1 = ['мир']
-IoC.resolve('IoC.Register', ['hi1', ()=> new Plugin1(args1[0])])
+IoC.resolve('IoC.Register', ['hi1', () => new Plugin1(args1[0])])
 const args2 = ['человек']
-IoC.resolve('IoC.Register', ['hi2', ()=> new Plugin2(args2[0])])
+IoC.resolve('IoC.Register', ['hi2', () => new Plugin2(args2[0])])
 
 const mainPage = new Page()
-const hi1 = new Component('hi1', {})
-const hi2 = new Component('hi2', {})
-mainPage.addComponent(hi1)
-mainPage.addComponent(hi2)
-mainPage.render();
+//TODO автоматическое добавление с помощью ? Page.getPlagin
+const plugin1: IPlugin = IoC.resolve('hi1', [])
+const plugin2: IPlugin = IoC.resolve('hi2', [])
+mainPage.addPlugin(plugin1)
+mainPage.addPlugin(plugin2)
+mainPage.render()
