5401874bf5ea68cf234476106da504aa1dfc93f7
i.egoshin <i.egoshin@redkit-lab.ru>
2024-08-13 17:06:05 +0500

    Docker

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const PLUGIN_REGISTRY_PATH = path.join(__dirname, 'publish', 'plugin-registry.json');
 
 // Middleware для парсинга JSON
